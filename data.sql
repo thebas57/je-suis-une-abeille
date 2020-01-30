18922aad1b6ba6a036d5049583d8e636b0883c68
thebas57 <theohelf@gmail.com>
2020-01-30 11:39:12 +0100

    insertion qques fleurs

---
+++ data.sql
@@ -0,0 +1,30 @@
+INSERT INTO emplacement (nom)
+ VALUES
+ ('collection historique'),
+ ('forêt'),
+ ('étang'),
+ ('collection ornementale'),
+ ('secteur arboretum Chine'),
+ ('secteur terre de bruyère'),
+ ('secteur arboretum Europe'),
+ ('arboretum'),
+ ('secteur Chine'),
+ ('pelouses'),
+ ('collection de dahlias'),
+ ('ruisseau'),
+ ('vert tendre'),
+ ('potager'),
+ ('potager écologique'),
+ ('roseraie'),
+ ('étang pavillon d``accueil'),
+ ('collection historique ornementale'),
+ ('pelouse prairie'),
+ ('jardin écologique'),
+ ('parcelle Emile Gallé');
+
+INSERT INTO fleurs (nomLatin,nomFr,pollen,nectar,floraison,couleur,hauteur,emplacement_id,illustration)
+VALUES
+('Acer campestris','érable champêtre','2','3','avril,mai','vert clair','15','1','Acer campestre.jpg'),
+('Acer pseudoplatanus','érable sycomore','2','1','mai','jaune','25','2','Acer pseudoplatanus.jpg'),
+('Alnus glutinosa','alune glutineux','2','1','mars,avril','jaune','25','3','Alnus glutinosa.jpg');
+
